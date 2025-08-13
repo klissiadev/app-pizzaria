d5e2a590cf32af06c90af185d421bfdbe4aab48e
rebeccajh-dev <rerdr.snf24@uea.edu.br>
2025-08-12 21:21:27 -0400

    adjusting background and some buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Box, CssBaseline, IconButton } from '@mui/material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
 import Header from './components/Header';
 import AppRoutes from './routes/AppRoutes';
+import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: darkMode ? 'dark' : 'light',
+          background: {
+            default: darkMode ? '#121212' : '#f5f5f5', // fundo global
+          },
+        },
+      }),
+    [darkMode]
+  );
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
   return (
-    <>
-      <Header/>
-      <AppRoutes/>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+
+      <Box
+        sx={{
+          position: 'fixed',
+          bottom: 16,
+          right: 16,
+          zIndex: 1300,
+          borderRadius: "50%",
+          boxShadow: 3,
+          backgroundColor: theme.palette.background.default,
+        }}
+      >
+        <IconButton onClick={toggleDarkMode} color="inherit">
+          {darkMode ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+      </Box>
+
+      {/* Conteúdo */}
+      <div>
+      </div>
+      <Header />
+      <AppRoutes />
+    </ThemeProvider>
   );
 }
 
--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -4,6 +4,7 @@ import {
   AppBar, Toolbar, Typography, Box, Button
 } from "@mui/material";
 import PizzaCard from "../components/PizzaCard";
+import { toast } from "react-toastify";
 
 const Cardapio = () => {
   const [pizzas, setPizzas] = useState([]);
@@ -22,6 +23,13 @@ const Cardapio = () => {
         ? prevFiltros.filter((filtroId) => filtroId !== id)
         : [...prevFiltros, id]
     );
+  };
+
+  const finalizarPedido = () => {
+    toast.sucess("Pedido finalizado com sucesso!", {
+      position: toast.position.top_right,
+      autoClose: 3000,
+    });
   };
 
   useEffect(() => {
@@ -47,13 +55,17 @@ const Cardapio = () => {
       : pizzas;
 
   return (
-    <Box sx={{ bgcolor: "#f5f5f5", minHeight: "100vh" }}>
+    <Box sx={(theme) => ({
+      bgcolor: theme.palette.background.default,
+      minHeight: "100vh",
+    })}>
       <AppBar position="static" sx={{ bgcolor: "#558858ff" }}>
         <Toolbar sx={{ justifyContent: "space-between" }}>
           <Typography variant="h6">Cardápio</Typography>
           <Button
             variant="contained"
-            sx={{ bgcolor: "#a5140a", "&:hover": { bgcolor: "#9a3730ff" } }}
+            sx={{ color: "#ffffff", bgcolor: "#a5140a", "&:hover": { bgcolor: "#9a3730ff" } }}
+            onClick={finalizarPedido}
           >
             Finalizar Pedido
           </Button>
@@ -61,25 +73,26 @@ const Cardapio = () => {
       </AppBar>
 
       <Box sx={{ display: "flex", gap: 2, p: 2, flexWrap: "wrap" }}>
-        {botoes.map(({ id, label }) => (
-          <Button
-            key={id}
-            variant={filtrosAtivos.includes(id) ? "contained" : "outlined"}
-            onClick={() => handleClickFiltro(id)}
-            sx={{
-              bgcolor: filtrosAtivos.includes(id) ? "#0d7212ff" : "#fafafaff",
-              color: filtrosAtivos.includes(id) ? "#ffffffff" : "#0d7212ff",
-              borderColor: "#0d7212ff",
-              borderRadius: "10px",
-              "&:hover": {
-                bgcolor: filtrosAtivos.includes(id) ? "#0f8c23" : "#e8f5e9",
-              },
-            }}
-          >
-            {label}
-          </Button>
-        ))}
-      </Box>
+  {botoes.map(({ id, label }) => (
+    <Button
+      key={id}
+      variant={filtrosAtivos.includes(id) ? "contained" : "outlined"}
+      onClick={() => handleClickFiltro(id)}
+      sx={(theme) => ({
+        bgcolor: filtrosAtivos.includes(id) ? "#0d7212ff" : theme.palette.background.default,
+        color: filtrosAtivos.includes(id) ? "#ffffff" : "#0d7212ff",
+        borderColor: "#0d7212ff",
+        borderRadius: "10px",
+        "&:hover": {
+          bgcolor: filtrosAtivos.includes(id) ? "#0f8c23" : "#e8f5e9",
+        },
+      })}
+    >
+      {label}
+    </Button>
+  ))}
+</Box>
+
 
       <PizzaCard pizzas={pizzasFiltradas} />
 
